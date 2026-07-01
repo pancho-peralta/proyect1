32f04cc9febec6b1f3d400838724dc9bf5c9ee16
pancho-peralta <chetofranh@gmail.com>
2026-07-01 16:16:11 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
 st.title("cris mj")
 st.image("https://static.ptocdn.net/images/eventos/pls018_rs.jpg")
 st.write("developer pancho")
+df = pd.read_csv("https://raw.githubusercontent.com/pancho-peralta/proyect1/refs/heads/main/train.csv")
+st.write(df)
